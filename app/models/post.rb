7e1c32bf5b38516fe6a4e8c9e644c6c95352c630
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-09-15 17:22:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
     include ActiveModel::Validations
     validates_with PostValidator
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
-    validates :category, inclusion: { in: [fiction, non-fiction] }
+    validates :category, inclusion: { in: ["Fiction", "Non-fiction"] }
 
 end
--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,5 +1,7 @@
 class PostValidator < ActiveModel::Validator
     def validate(record)
-    
-    end
+        if record.title && !record.title.match(/Won't Believe|Secret|Top [\d*]|Guess/)
+            record.errors[:title] << "This might not be clickbait-y!"
+        end
+    end  
 end
