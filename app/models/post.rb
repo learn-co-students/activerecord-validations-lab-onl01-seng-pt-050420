4e17926f6a9242fdcaebdd6446d865eeb6390937
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-09-15 16:32:59 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with PostValidator
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: [fiction, non-fiction] }
+
 end
