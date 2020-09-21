0db42e015b54556f5ab7b7cb7415f9aa3608c281
dangwin <danghwin@gmail.com>
2020-09-21 17:54:41 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 10}
+    validates :summary, length: {maximum: 100}
+    validates :category, inclusion: {in: %w(Fiction)}
+    validate :click_bait
+    
+
+    def click_bait
+        if title.present? && !["Won't Believe", "Secret", "Top [number]", "Guess"].any? {|t| title.include? t}
+            errors.add(:title, "Not Click Bait")
+        end
+    end
 end
